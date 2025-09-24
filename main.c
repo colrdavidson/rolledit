8fffdfa353921acc8edc082eeea0fa223f1dfe8d
Colin Davidson <colrdavidson@gmail.com>
2025-09-24 14:36:47 -0700

    add cleanup, add frame and sample catchup

--- main.c
+++ main.c
@@ -21,6 +21,8 @@
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
+bool quit = false;
+
 typedef struct {
 	void **list;
 	int64_t size;
@@ -94,6 +96,13 @@ void *queue_peek(Queue *q) {
 	return data;
 }
 
+int64_t queue_size(Queue *q) {
+	pthread_mutex_lock(&q->lock);
+	int64_t cur_size = (int64_t)q->tail - (int64_t)q->head;
+	pthread_mutex_unlock(&q->lock);
+	return cur_size;
+}
+
 typedef struct {
 	uint8_t *data;
 	uint64_t size;
@@ -109,6 +118,10 @@ typedef struct {
 
 typedef struct {
 	char *filename;
+
+	bool pause;
+	int64_t start_time;
+	double frame_rate;
 	int64_t audio_idx;
 
 	int width;
@@ -128,36 +141,66 @@ void sleep_ns(uint64_t ns) {
 	nanosleep(&requested_time, &remaining_time);
 }
 
+void free_frame(Frame *frame) {
+	free(frame->pixels);
+	free(frame);
+}
+
+void free_sample(Sample *sample) {
+	free(sample->data);
+	free(sample);
+}
+
 void audio_callback(void *userdata, uint8_t *stream, int len) {
 	PlaybackState *pb = (PlaybackState *)userdata;
+	memset(stream, 0, len);
 
-	Sample *sample = (Sample *)queue_pop(&pb->samples);
-	if (sample != NULL) {
-		uint64_t rem_len = MIN(len, sample->size);
-		if (len != rem_len) {
-			printf("flushing audio buffer tail\n");
-			memset(stream + rem_len, 0, len - rem_len);
+	if (pb->pause) {
+		return;
+	}
+
+	int64_t cur_time_us = av_gettime();
+	int64_t rescaled_time_us = cur_time_us - pb->start_time;
+
+	int64_t skip_count = 0;
+	Sample *cur_sample = NULL;
+	while (queue_size(&pb->samples) > 0) {
+		Sample *next_sample = (Sample *)queue_peek(&pb->samples);
+		if (next_sample->pts_us > rescaled_time_us) {
+			break;
+		} else {
+			if (cur_sample) {
+				skip_count += 1;
+				free_sample(cur_sample);
+			}
+			cur_sample = (Sample *)queue_pop(&pb->samples);
 		}
-		memcpy(stream, sample->data, rem_len);
+	}
+	
+	if (skip_count > 0) {
+		printf("skipping %lld samples\n", skip_count);
+	}
+
+	if (cur_sample) {
+		uint64_t rem_len = MIN(len, cur_sample->size);
+		memcpy(stream, cur_sample->data, rem_len);
 
 		pb->audio_idx += rem_len;
 
-		free(sample->data);
-		free(sample);
+		free_sample(cur_sample);
 	} else {
 		printf("nothing to grab\n");
-		memset(stream, 0, len);
 	}
 
 	return;
 }
 
 void *decode_video(void *userdata) {
-	PlaybackState *state = (PlaybackState *)userdata;
+	PlaybackState *pb = (PlaybackState *)userdata;
 
 	AVFormatContext *fmt_ctx = NULL;
-	if (avformat_open_input(&fmt_ctx, state->filename, NULL, NULL) < 0) {
-		printf("unable to open clip %s\n", state->filename);
+	if (avformat_open_input(&fmt_ctx, pb->filename, NULL, NULL) < 0) {
+		printf("unable to open clip %s\n", pb->filename);
 		return NULL;
 	}
 
@@ -202,12 +245,12 @@ void *decode_video(void *userdata) {
 	}
 
 	AVChannelLayout out_layout;
-	av_channel_layout_default(&out_layout, state->channels);
+	av_channel_layout_default(&out_layout, pb->channels);
 
 	SwrContext *swr = NULL;
 	if (swr_alloc_set_opts2(
 		&swr,
-		&out_layout, state->sample_fmt, state->sample_rate,
+		&out_layout, pb->sample_fmt, pb->sample_rate,
 		&audio_ctx->ch_layout, audio_ctx->sample_fmt, audio_ctx->sample_rate,
 		0, NULL
 	) < 0) {
@@ -231,12 +274,13 @@ void *decode_video(void *userdata) {
 		printf("could not open video codec\n");
 		return NULL;
 	}
+	pb->frame_rate = av_q2d(video_stream->r_frame_rate);
 
-	struct SwsContext *sws_ctx = sws_getContext(video_ctx->width, video_ctx->height, video_ctx->pix_fmt, state->width, state->height, AV_PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL);
+	struct SwsContext *sws_ctx = sws_getContext(video_ctx->width, video_ctx->height, video_ctx->pix_fmt, pb->width, pb->height, AV_PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL);
 
 	AVPacket *pkt = av_packet_alloc();
 	AVFrame *frame = av_frame_alloc();
-	while (av_read_frame(fmt_ctx, pkt) >= 0) {
+	while (av_read_frame(fmt_ctx, pkt) >= 0 && !quit) {
 		if (pkt->stream_index == video_stream_idx) {
 			if (avcodec_send_packet(video_ctx, pkt) < 0) {
 				printf("Error sending video packet for decoding\n");
@@ -258,16 +302,19 @@ void *decode_video(void *userdata) {
 					pts = av_rescale_q(frame->best_effort_timestamp, video_stream->time_base, AV_TIME_BASE_Q);
 				}
 
-				int num_bytes = av_image_get_buffer_size(AV_PIX_FMT_YUV420P, state->width, state->height, 32);
+				int num_bytes = av_image_get_buffer_size(AV_PIX_FMT_YUV420P, pb->width, pb->height, 32);
 				Frame *f = (Frame *)calloc(1, sizeof(Frame));
 				f->pts_us = pts;
 				f->pixels = malloc(num_bytes);
 
-				av_image_fill_arrays(f->buffers, f->strides, f->pixels, AV_PIX_FMT_YUV420P, state->width, state->height, 32);
+				av_image_fill_arrays(f->buffers, f->strides, f->pixels, AV_PIX_FMT_YUV420P, pb->width, pb->height, 32);
 				sws_scale(sws_ctx, (uint8_t const * const *)frame->data, frame->linesize, 0, video_ctx->height, f->buffers, f->strides);
 
-				while (!queue_push(&state->frames, (void *)f)) {
+				while (!queue_push(&pb->frames, (void *)f) && !quit) {
 					sleep_ns(10000);
+				}
+				if (quit) {
+					return NULL;
 				}
 			} while (ret >= 0);
 		} else if (pkt->stream_index == audio_stream_idx) {
@@ -290,28 +337,29 @@ void *decode_video(void *userdata) {
 				if (frame->best_effort_timestamp != AV_NOPTS_VALUE) {
 					pts = av_rescale_q(frame->best_effort_timestamp, audio_stream->time_base, AV_TIME_BASE_Q);
 				}
-				if (state->sample_rate != audio_ctx->sample_rate) {
+				if (pb->sample_rate != audio_ctx->sample_rate) {
 					printf("TODO: pts calc needs to get fixed for new sample rate\n");
+					printf("pb rate: %d != ctx rate: %d\n", pb->sample_rate, audio_ctx->sample_rate);
 					exit(1);
 				}
 
 				int dst_samples = av_rescale_rnd(
 					swr_get_delay(swr, audio_ctx->sample_rate) + frame->nb_samples,
-					state->sample_rate,
+					pb->sample_rate,
 					audio_ctx->sample_rate,
 					AV_ROUND_UP
 				);
 
-				int audio_buf_size = av_samples_get_buffer_size(NULL, state->channels, dst_samples, state->sample_fmt, 1);
+				int audio_buf_size = av_samples_get_buffer_size(NULL, pb->channels, dst_samples, pb->sample_fmt, 1);
 				uint8_t *audio_buf = calloc(1, audio_buf_size);
 
-				int dst_chan_sample_count = swr_convert(swr, &audio_buf, dst_samples * state->channels, (const uint8_t **)frame->data, frame->nb_samples);
+				int dst_chan_sample_count = swr_convert(swr, &audio_buf, dst_samples * pb->channels, (const uint8_t **)frame->data, frame->nb_samples);
 				if (dst_chan_sample_count < 0) {
 					printf("Failed to convert samples\n");
 					return NULL;
 				}
-				int sample_bytes = av_get_bytes_per_sample(state->sample_fmt);
-				int total_size_bytes = dst_chan_sample_count * state->channels * sample_bytes;
+				int sample_bytes = av_get_bytes_per_sample(pb->sample_fmt);
+				int total_size_bytes = dst_chan_sample_count * pb->channels * sample_bytes;
 
 				Sample *s = (Sample *)malloc(sizeof(Sample));
 				*s = (Sample){
@@ -320,8 +368,11 @@ void *decode_video(void *userdata) {
 					.pts_us = pts
 				};
 
-				while (!queue_push(&state->samples, (void *)s)) {
+				while (!queue_push(&pb->samples, (void *)s) && !quit) {
 					sleep_ns(10000);
+				}
+				if (quit) {
+					return NULL;
 				}
 			} while (ret >= 0);
 		} else {
@@ -381,36 +432,71 @@ int main(int argc, char **argv) {
 
 	pthread_t decode_thread;
 	pthread_create(&decode_thread, NULL, decode_video, (void *)&pb);
-	int64_t start_time = av_gettime();
+	pb.start_time = av_gettime();
 
+	int64_t cur_time_us = av_gettime();
 	for (;;) {
-		int64_t cur_time_us = av_gettime();
-		int64_t rescaled_time_us = cur_time_us - start_time;
+		if (!pb.pause) {
+			cur_time_us = av_gettime();
+		}
+		int64_t rescaled_time_us = cur_time_us - pb.start_time;
 
 		SDL_Event event;
 		SDL_PollEvent(&event);
 		switch (event.type) {
 			case SDL_QUIT: {
-				SDL_Quit();
-				return 0;
+				quit = true;
+				goto end;
 			} break;
 		}
 
-
-		Frame *frame = (Frame *)queue_peek(&pb.frames);
-		if (frame != NULL) {
-			if (rescaled_time_us >= frame->pts_us) {
-				SDL_Rect rect = (SDL_Rect){.x = 0, .y = 0, .w = pb.width, .h = pb.height};
-				SDL_UpdateYUVTexture(texture, &rect,
-					frame->buffers[0], frame->strides[0],
-					frame->buffers[1], frame->strides[1],
-					frame->buffers[2], frame->strides[2]
-				);
-
-				free(frame->pixels);
-				Frame *used_frame = queue_pop(&pb.frames);
-				free(used_frame);
+		int64_t skip_count = 0;
+		Frame *cur_frame = NULL;
+		while (queue_size(&pb.frames) > 0) {
+			Frame *next_frame = (Frame *)queue_peek(&pb.frames);
+			if (next_frame->pts_us > rescaled_time_us) {
+				break;
+			} else {
+				if (cur_frame) {
+					skip_count += 1;
+					free_frame(cur_frame);
+				}
+				cur_frame = (Frame *)queue_pop(&pb.frames);
 			}
+		}
+		
+		if (skip_count > 0) {
+			printf("skipping %lld frames\n", skip_count);
+		}
+
+		if (cur_frame && !pb.pause) {
+			SDL_Rect rect = (SDL_Rect){.x = 0, .y = 0, .w = pb.width, .h = pb.height};
+			SDL_UpdateYUVTexture(texture, &rect,
+				cur_frame->buffers[0], cur_frame->strides[0],
+				cur_frame->buffers[1], cur_frame->strides[1],
+				cur_frame->buffers[2], cur_frame->strides[2]
+			);
+
+			free_frame(cur_frame);
+		}
+
+		int64_t frame = 0;
+		if (pb.frame_rate != 0) {
+			double us_per_frame = 1000000.0 / pb.frame_rate;
+			frame = (int64_t)((double)rescaled_time_us / us_per_frame);
+		}
+
+		int sample_bytes = av_get_bytes_per_sample(pb.sample_fmt);
+		int64_t bytes_per_s = pb.sample_rate * sample_bytes * pb.channels;
+		double audio_clock_s = (double)pb.audio_idx / (double)bytes_per_s;
+
+		if (pb.pause) {
+			continue;
+		}
+
+		if (frame == 493) {
+			pb.pause = true;
+			printf("%08lld | frame %lld, sample %f s\n", rescaled_time_us, frame, audio_clock_s);
 		}
 
 		SDL_RenderClear(renderer);
@@ -418,5 +504,8 @@ int main(int argc, char **argv) {
 		SDL_RenderPresent(renderer);
 	}
 
+end:
+	pthread_join(decode_thread, NULL);
+	SDL_Quit();
 	return 0;
 }
